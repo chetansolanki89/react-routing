fe110c613e607c50e6186719e36a9994f46c594e
Chetan R. Solanki <chetansolanki.89@gmail.com>
2022-02-24 17:18:43 +0530

    Reorganize

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-routing
- Codevolution practice example
--- src/components/NoMatch.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const NoMatch = () => {
-  return <h1>Page not found!</h1>;
-};
-
-export default NoMatch;
